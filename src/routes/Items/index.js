e44ad76a69a49a07bc6048877d9054c8738f436c
xwatson <xwatson@foxmail.com>
2016-12-15 14:51:25 +0800

    update zen

--- src/routes/Items/index.js
+++ src/routes/Items/index.js
@@ -7,10 +7,10 @@ export default (store) => ({
     path: 'items',
     getComponent(nextState, cb) {
         require.ensure([], (require) => {
-            const Zen = require('./containers/ItemsContainer').default
+            const Items = require('./containers/ItemsContainer').default
             const reducer = require('./modules/items').default
-            injectReducer(store, { key: 'items', reducer })
-            cb(null, Zen)
+            injectReducer(store, { key: 'Items', reducer })
+            cb(null, Items)
         })
     }
 })
