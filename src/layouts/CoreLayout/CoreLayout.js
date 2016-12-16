618d8333f891a0d7222b052b0b73d325f7d1826b
xwatson <xwatson@foxmail.com>
2016-12-16 18:45:16 +0800

    add internationalization

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -2,33 +2,68 @@
  * Created by xwatson on 2016/12/8.
  */
 import React from 'react'
+import { browserHistory } from 'react-router'
+import { LocaleProvider, Radio } from 'antd'
 import LeftMenu from '../../components/LeftMenu'
 import Breadcrumb from '../../components/Breadcrumb'
 import './CoreLayout.scss'
 import '../../styles/main.scss'
+import enUS from 'antd/lib/locale-provider/en_US'
+import moment from 'moment'
+import 'moment/locale/zh-cn'
+moment.locale('en')
 
-export const CoreLayout = ({ children }) => (
-    <div className="ant-layout-aside">
-        <aside className="ant-layout-sider">
-            <div className="ant-layout-logo">&nbsp;</div>
-            <LeftMenu />
-        </aside>
-        <div className="ant-layout-main">
-            <div className="ant-layout-header">Header</div>
-            <div className="ant-layout-breadcrumb">
-                <Breadcrumb />
-            </div>
-            <div className="ant-layout-container">
-                <div className="ant-layout-content">
-                    {children}
+export class CoreLayout extends React.Component {
+    constructor(props) {
+        super(props)
+        this.changeLocale = this.changeLocale.bind(this)
+    }
+    state = {
+        locale: enUS
+    }
+    changeLocale(e) {
+        const localeValue = e.target.value
+        this.setState({ locale: localeValue })
+        if (!localeValue) {
+            moment.locale('zh-cn')
+        } else {
+            moment.locale('en')
+        }
+        browserHistory.replace(browserHistory.getCurrentLocation())
+    }
+    render() {
+        return (
+            <div className="ant-layout-aside">
+                <aside className="ant-layout-sider">
+                    <div className="ant-layout-logo">&nbsp;</div>
+                    <LeftMenu />
+                </aside>
+                <div className="ant-layout-main">
+                    <div className="ant-layout-header">
+                        <Radio.Group style={{ float:'right' }} defaultValue={enUS} onChange={this.changeLocale}>
+                            <Radio.Button key="en" value={enUS}>English</Radio.Button>
+                            <Radio.Button key="cn">中文</Radio.Button>
+                        </Radio.Group>
+                    </div>
+                    <div className="ant-layout-breadcrumb">
+                        <Breadcrumb />
+                    </div>
+                    <div className="ant-layout-container">
+                        <div className="ant-layout-content">
+                            <LocaleProvider locale={this.state.locale}>
+                                {this.props.children}
+                            </LocaleProvider>
+
+                        </div>
+                    </div>
+                    <div className="ant-layout-footer">
+                        OBORCloud 版权所有 © 2016
+                    </div>
                 </div>
             </div>
-            <div className="ant-layout-footer">
-                OBORCloud 版权所有 © 2016
-            </div>
-        </div>
-    </div>
-)
+        )
+    }
+}
 
 CoreLayout.propTypes = {
     children: React.PropTypes.element.isRequired
