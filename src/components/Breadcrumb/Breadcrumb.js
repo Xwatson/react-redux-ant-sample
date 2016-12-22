b4e9771199f9dc9e56f6add8b1be4389ba0c1a23
xwatson <xwatson@foxmail.com>
2016-12-22 17:02:08 +0800

    优化代码

--- src/components/Breadcrumb/Breadcrumb.js
+++ src/components/Breadcrumb/Breadcrumb.js
@@ -17,19 +17,16 @@ export default class Breadcrumbs extends React.Component {
         let isDetail = match && match[3]
         let _bread = [<Breadcrumb.Item key={'bread-0'}>{match ? <Link to="/">首页</Link> : '首页'}</Breadcrumb.Item>]
         let subs = this.props.menus.subs
-        let count = 1
-        for (let key in subs) {
-            let items = subs[key].items
-            for (let item in items) {
-                if (items[item].router === path) {
-                    _bread.push(<Breadcrumb.Item key={'bread-' + count}>{!isDetail ? items[item].name : <Link to={items[item].router}>{items[item].name}</Link>}</Breadcrumb.Item>)
+        subs.forEach(function(sub) {
+            sub.items.forEach(function(item, i) {
+                if (item.router === path) {
+                    _bread.push(<Breadcrumb.Item key={'bread-' + i}>{!isDetail ? item.name : <Link to={item.router}>{item.name}</Link>}</Breadcrumb.Item>)
                     if (isDetail) {
-                        _bread.push(<Breadcrumb.Item key={'bread-' + count}>{items[item].name}详情</Breadcrumb.Item>)
+                        _bread.push(<Breadcrumb.Item key={'bread-' + i}>{item.name}详情</Breadcrumb.Item>)
                     }
-                    count++
                 }
-            }
-        }
+            })
+        })
         return _bread
     }
     render() {
